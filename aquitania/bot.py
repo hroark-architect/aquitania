0aca026c40c3b9e93ba57f6cbebea6d4d35a9c67
Howard Roark <hroark.aquitania@gmail.com>
2018-06-12 05:12:06 -0300

    Bot: Correcting asset offset [BUG FIXING]

--- aquitania/bot.py
+++ aquitania/bot.py
@@ -345,7 +345,7 @@ def arg_parser():
     parser.add_argument('-a', '--assets', type=int, metavar='', help='Number of assets')
 
     # Selects number of assets
-    parser.add_argument('-ao', '--assetsoffset', type=int, metavar='', help='Number of assets offset')
+    parser.add_argument('-ao', '--assetoffset', type=int, metavar='', help='Number of assets offset')
 
     # Selects DataSource name
     parser.add_argument('-s', '--source', type=str, metavar='', help='Data Source name')
@@ -493,7 +493,7 @@ if __name__ == '__main__':
     n_assets = args.assets if args.assets is not None else config.getint('settings', 'n_assets')
 
     # Gets number of assets
-    asset_offset = args.asset_offset if args.asset_offset is not None else config.getint('settings', 'asset_offset')
+    asset_offset = args.asset_offset if args.assetoffset is not None else config.getint('settings', 'asset_offset')
 
     # Generates list of assets
     asset_list = ref.cur_ordered_by_spread[asset_offset:n_assets + asset_offset]
